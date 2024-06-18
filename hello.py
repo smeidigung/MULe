ff7341036d9f7b38f30a35245b799ac00cbd0843
erik <smede_gaard@hotmail.com>
2024-06-18 15:01:24 +0200

    Create site menu with navigation bar (#3)
    * Add blank subpage templates and menu. Error in flask.Markup
    
    Add blank subpage templates and top navigation menu.
    Receiving error when running due to function "Markup" in flask library.
    
    * savepoint for navigation
    
    * Implement navigation bar and base html
    
    Implemented navigation bar and a base html where all subpages inherit from, including navigation bar and banner.
    
    * Remove submenu and unused code

--- hello.py
+++ hello.py
@@ -1,18 +1,56 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+@app.context_processor 
+def inject_dict_for_all_templates():
+    """ Build the navbar and pass this to Jinja for every route
+    """
+    # Build the Navigation Bar
+    nav = [
+    {"text": "Homepage", "url": url_for('index')},
+    {"text": "My Account", "url": url_for('account')},
+    {"text": "About MULe", "url": url_for('about')},
+    {"text": "Contact", "url": url_for('contact')},
+    {"text": "My Signup", "url": url_for('signup')},
+    {"text": "Practical Info", "url": url_for('practical_info')},
+    {"text": "FAQ", "url": url_for('FAQ')},
+    ]
+
+    return dict(navbar = nav)
+
+
+
 @app.route("/")
-def home():
-    return "<p>Hello!</p>"
+def index():
+    return render_template("index.html")
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin"))
+@app.route('/myaccount')
+def account():
+    return render_template('my_account.html')
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/info')
+def practical_info():
+    return render_template('practical_info.html')
+
+@app.route('/faq')
+def FAQ():
+    return render_template('FAQ.html')
+
+if __name__ == "__main__":
+    app.run
 
 #if __name__ == "__main__":
 #    app.run(host="0.0.0.0")
